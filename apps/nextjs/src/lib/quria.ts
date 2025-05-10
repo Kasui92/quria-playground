987d9bdd6c782fbd0b886bcfc6b4d476dbd42a9b
Luca Pattocchio <pattocchioluca@gmail.com>
2025-05-10 23:54:14 +0200

    fix: split CSR from SSR for quria configuration

--- apps/nextjs/src/lib/quria.ts
+++ apps/nextjs/src/lib/quria.ts
@@ -1,7 +1,13 @@
 import { Quria } from "quria";
 
-export const quria = new Quria({
+export const quriaSSR = new Quria({
   API_KEY: process.env.QURIA_API_KEY || "",
   CLIENT_ID: process.env.QURIA_CLIENT_ID || "",
   CLIENT_SECRET: process.env.QURIA_CLIENT_SECRET || "",
+});
+
+export const quriaCSR = new Quria({
+  API_KEY: process.env.NEXT_PUBLIC_QURIA_API_KEY || "",
+  CLIENT_ID: process.env.NEXT_PUBLIC_QURIA_CLIENT_ID || "",
+  CLIENT_SECRET: process.env.NEXT_PUBLIC_QURIA_CLIENT_SECRET || "",
 });
